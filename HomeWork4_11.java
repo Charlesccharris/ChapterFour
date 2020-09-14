ab05179414e71805fc79c14ae1e9df65c5faeb52
Charlesccharris <harriscc@student.swosu.edu>
2020-09-14 11:03:19 -0500

    Added the project information

--- HomeWork4_11.java
+++ HomeWork4_11.java
@@ -1,5 +1,13 @@
 class HomeWork4_11{
 	public static void main(String[] args){
+	/*(Decimal to hex)
+	Write a program that prompts the user to enter an integer between 0 and 15 and displays its corresponding hex number.
+	Here are some sample runs:
+	Enter a decimal value (0 to 15): 11 The hex value is B;
+	Enter a decimal value (0 to 15): 5; The hex value is 5;
+	Enter a decimal value (0 to 15): 31 31 is an invalid input*/
+
 	System.out.println("Hi");
+
 	}
 }
