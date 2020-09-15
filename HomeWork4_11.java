9c75a33db682c70f36ab3d8b791aacd9b6e7daf5
Charlesccharris <harriscc@student.swosu.edu>
2020-09-15 13:04:23 -0500

    Added steps to solve the problem

--- HomeWork4_11.java
+++ HomeWork4_11.java
@@ -1,3 +1,11 @@
+//1. Identify the problem
+//2. Copy and paste code
+//3. Make sure code works
+//4. Identify what needs to be modified
+//5. Make the modifications
+//6. Check to see if code works
+
+
 class HomeWork4_11{
 	public static void main(String[] args){
 	/*(Decimal to hex)
--- HomeWork4_22.java
+++ HomeWork4_22.java
@@ -1,3 +1,10 @@
+//1. Identify the problem
+//2. Copy and paste code
+//3. Make sure code works
+//4. Identify what needs to be modified
+//5. Make the modifications
+//6. Check to see if code works
+
 class HomeWork4_22{
 	public static void main(String[] args){
 	System.out.println("Hi");
