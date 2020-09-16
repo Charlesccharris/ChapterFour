0f84edf5c1b71a1ab95ee3bf55f6b8360419dcca
Charlesccharris <harriscc@student.swosu.edu>
2020-09-16 17:21:01 -0500

    Changed the wording to sound better

--- HomeWork4_22.java
+++ HomeWork4_22.java
@@ -5,24 +5,27 @@
 //5. Make the modifications
 //6. Check to see if code works
 
+import java.util.Scanner;
+
 class HomeWork4_22{
-	public static void main(String[] args){
-	/*(Check substring)
-	Write a program that prompts the user to enter two strings and reports whether the second string is a substring of the first string.
-	Here is a sample run:
-	Enter string s1: ABCD;
-	Enter string s2: BC;
-	BC is a substring of ABCD*/
-	String inputOne = "ABCD";
-	String inputTwo = "CD";
+        public static void main(String[] args){
+        /*(Check substring)
+        Write a program that prompts the user to enter two strings and reports whether the second string is a substring of the first string.
+        Here is a sample run:
+        Enter string s1: ABCD;
+        Enter string s2: BC;
+        BC is a substring of ABCD*/
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a string of letters(capitalization matters): ");
+        String inputOne = input.next();
+	System.out.print("\nInput a smaller string of letters to see if it's a substring(capitalization matters): ");
+        String inputTwo = input.next();
 
-	System.out.println("inputOne is " + inputOne + " and inputTwo is " + inputTwo);
-	if(inputOne.contains(inputTwo))
-	System.out.println("Hi");
-	else
-	System.out.println("bye");
+        if(inputOne.contains(inputTwo))
+          System.out.println("\n" + inputTwo + " is a substring of " + inputOne);
+        else
+          System.out.println("\n" + inputTwo + " isn't a substring of " + inputOne);
 
-//	String(userInput).equals(send);
-//	answer.equals "Yes";
-	}
+        }
 }
+
