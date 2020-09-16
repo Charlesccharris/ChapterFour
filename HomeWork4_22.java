31013cdd7ae8317eaa8e5e108661e6a1c3166efa
Charlesccharris <harriscc@student.swosu.edu>
2020-09-16 15:39:43 -0500

    Got it working

--- HomeWork4_22.java
+++ HomeWork4_22.java
@@ -7,6 +7,22 @@
 
 class HomeWork4_22{
 	public static void main(String[] args){
+	/*(Check substring)
+	Write a program that prompts the user to enter two strings and reports whether the second string is a substring of the first string.
+	Here is a sample run:
+	Enter string s1: ABCD;
+	Enter string s2: BC;
+	BC is a substring of ABCD*/
+	String inputOne = "ABCD";
+	String inputTwo = "CD";
+
+	System.out.println("inputOne is " + inputOne + " and inputTwo is " + inputTwo);
+	if(inputOne.contains(inputTwo))
 	System.out.println("Hi");
+	else
+	System.out.println("bye");
+
+//	String(userInput).equals(send);
+//	answer.equals "Yes";
 	}
 }
